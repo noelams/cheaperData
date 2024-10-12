7ce23e4707e05714a3f0817b26cb859f09063972
Ali Noel <noelams03@gmail.com>
2024-10-12 13:13:44 -0700

    updated subcription.jsx

--- frontend/src/Components/Subcription.jsx
+++ frontend/src/Components/Subcription.jsx
@@ -1,27 +1,37 @@
 import React, { useState } from "react";
-import dataPlans from '../data/dataPlans.json'
-import './styles/Subcription.css'
+import dataPlans from "../data/dataPlans.json";
+import "./styles/Subcription.css";
 
 function Subcription() {
-  const [selectedProvider, setSelectedProvider] = useState('')
-  const [dataPlan, setDataPlan] = useState('')
+  const [selectedProvider, setSelectedProvider] = useState("");
+  const [dataPlan, setDataPlan] = useState("");
 
   const handleSelectChange = (event) => {
     console.log(event.target.value);
-    setSelectedProvider(event.target.value)
+    setSelectedProvider(event.target.value);
   };
 
-  const handleDataPlanChange = (event) =>{
-    setDataPlan(event.target.value)
-  }
+  const handleDataPlanChange = (event) => {
+    setDataPlan(event.target.value);
+  };
 
-  const networkProviders = Object.keys(dataPlans)
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const networkProviders = Object.keys(dataPlans);
 
   return (
-    <form action="" className="form-container">
+    <form action="" className="form-container" onSubmit={handleSubmit}>
       <label htmlFor="networkProviders"></label>
-      <select name="networkProviders" className="dropdown-menu" onChange={handleSelectChange}>
-        <option disabled value={''}>--select an option--</option>
+      <select
+        name="networkProviders"
+        className="dropdown-menu"
+        onChange={handleSelectChange}
+      >
+        <option disabled value={""}>
+          --select an option--
+        </option>
         {networkProviders.map((network, index) => {
           return (
             <option key={index} value={network} className="option">
@@ -30,11 +40,26 @@ function Subcription() {
           );
         })}
       </select>
-      <select name="dataPlans" disabled={!selectedProvider} className="dropdown-menu" onChange={handleDataPlanChange}>
-        <option value="" disabled>--select a plan--</option>
-        {selectedProvider && dataPlans[selectedProvider].map((plan) =>  {return <option key={plan.planId} value={plan.plamId}>{`${plan.amount} for ${plan.dataSize} - ${plan.validityDays} days`}</option>})}
+      <select
+        name="dataPlans"
+        disabled={!selectedProvider}
+        className="dropdown-menu"
+        onChange={handleDataPlanChange}
+      >
+        <option value="" disabled>
+          --select a plan--
+        </option>
+        {selectedProvider &&
+          dataPlans[selectedProvider].map((plan) => {
+            return (
+              <option
+                key={plan.planId}
+                value={plan.plamId}
+              >{`${plan.amount} - ${plan.dataSize} - ${plan.validityDays} days`}</option>
+            );
+          })}
       </select>
-      <input type="number" placeholder="Phone Number"/>
+      <input type="number" placeholder="Phone Number" />
       <button className="subcribe-button">subcribe</button>
     </form>
   );
